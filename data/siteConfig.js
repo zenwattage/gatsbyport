7e2181bc330483d4818818827d9010b596cfd07c
Scott H <zenwattage@gmail.com>
2020-01-29 16:44:26 -0800

    initial commit of portfolio

---
+++ data/siteConfig.js
@@ -0,0 +1,94 @@
+module.exports = {
+  siteTitle: 'Hi! I\'m Scott!',
+  siteDescription: `Create your online curriculum in just a few minutes with this starter`,
+  keyWords: ['gatsbyjs', 'react', 'curriculum'],
+  authorName: 'Scott Hansford',
+  githubUsername: 'zenwattage',
+  authorAvatar: '/images/avatar.jpeg',
+  authorDescription: `Designer & Developer. My beginning as a ,'Web Designer' was making Geocities pages for various hobbies and interests in high school. <br />
+    In 2013 I started working as a support technician for REI and was quickly promoted due to being able to adapt and learn technologies quickly. Though that position was temporary I highly enjoyed the process of collaborating with product experts to solve customer issues with technology.
+    In 2016, I began taking Computer Science courses and have completed CS courses Programming I & II, in Python, Java, SQL and Web Authoring(HTML,CSS/JS). I recently completed a 24 week Full Stack Web Development Certificate through the University of Washington.<br />
+    I currently working freelance while pursuing a position as a full-time frontend developer and mainly work with <strong>Javascript, NodeJS, React</strong> and am currently learning <strong>Gatsby</strong>`,
+  skills: [
+    {
+      name: 'HTML',
+      level: 90
+    },
+    {
+      name: 'CSS',
+      level: 96
+    },
+    {
+      name: 'Javascript',
+      level: 95
+    },
+    {
+      name: 'NodeJs',
+      level: 65
+    },
+    {
+      name: 'React',
+      level: 75
+    },
+    {
+      name: 'Git',
+      level: 85
+    },
+    {
+      name: 'Adobe Creative Suite',
+      level: 100
+    }
+  ],
+  jobs: [
+    {
+      company: "Surly Design Company",
+      begin: {
+        month: 'Aug',
+        year: '2019'
+      },
+      duration: null,
+      occupation: "Web Design and Graphic Design",
+      description: "Freelance: Brand, Product, and Web Designer"
+
+    }, {
+      company: "Anapro",
+      begin: {
+        month: 'dec',
+        year: '2016'
+      },
+      duration: '1 yr e 5 mos',
+      occupation: "Fullstack developer",
+      description: "Development and maintenance, corrective and preventive, of web applications for the real estate market."
+    }, {
+      company: "Anapro",
+      begin: {
+        month: 'set',
+        year: '2012'
+      },
+      duration: '4 yrs e 3 mos',
+      occupation: "Support Technician",
+      description: "Responsible for the implementation and parameterization of the system, training and customer support. Acting also in person in real estate launches guaranteeing the success and good use of the tool."
+
+    },
+    /* ... */
+  ],
+  social: {
+    linkedin: "https://www.linkedin.com/in/scott-hansford-239379130/",
+    github: "https://github.com/zenwattage",
+    email: "scott.hansford1775@gmail.com"
+  },
+  siteUrl: 'https://santosfrancisco.github.io/gatsbystarter-cv',
+  pathPrefix: '/gatsby-starter-cv', // Note: it must *not* have a trailing slash.
+  siteCover: '/images/rocksdark.png',
+  googleAnalyticsId: 'UA-154777602-1',
+  background_color: '#ffffff',
+  theme_color: '#25303B',
+  display: 'minimal-ui',
+  icon: 'src/assets/gatsby-icon.png',
+  headerLinks: [
+    {
+      label: 'Scott Hansford',
+      url: '/',
+    }
+  ]
+}
